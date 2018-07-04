218958dacdbf01493b746370109328d506b3019f
tucan9389 <tucan.dev@gmail.com>
2018-07-05 00:55:09 +0900

    first commit

---
+++ PoseEstimation-CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoseEstimation-CoreML
+//
+//  Created by GwakDoyoung on 05/07/2018.
+//  Copyright © 2018 tucan9389. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
