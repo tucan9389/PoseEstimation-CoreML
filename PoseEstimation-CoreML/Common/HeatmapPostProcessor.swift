8e985fcf25a8b4007fc5b10cb9f3d9b0b04fc534
tucan9389 <tucan9389@gmail.com>
2019-08-14 08:50:29 +0900

    Implement pose capturing

--- PoseEstimation-CoreML/Common/HeatmapPostProcessor.swift
+++ PoseEstimation-CoreML/Common/HeatmapPostProcessor.swift
@@ -10,16 +10,24 @@ import Foundation
 import CoreML
 
 class HeatmapPostProcessor {
-    func convertToPredictedPoints(from heatmaps: MLMultiArray) -> [PredictedPoint?] {
+    
+    //var maxvalue: Double = 0.5
+    //var minvalue: Double = 0.5
+    
+    var onlyBust: Bool = false
+    
+    func convertToPredictedPoints(from heatmaps: MLMultiArray, isFlipped: Bool = false) -> [PredictedPoint?] {
         guard heatmaps.shape.count >= 3 else {
             print("heatmap's shape is invalid. \(heatmaps.shape)")
             return []
         }
-        let keypoint_number = heatmaps.shape[0].intValue
+        let total_keypoint_number = heatmaps.shape[0].intValue
+        var keypoint_number = total_keypoint_number
+        if onlyBust { keypoint_number = min(total_keypoint_number, 8/*the index of R hip*/) }
         let heatmap_w = heatmaps.shape[1].intValue
         let heatmap_h = heatmaps.shape[2].intValue
         
-        var n_kpoints = (0..<keypoint_number).map { _ -> PredictedPoint? in
+        var n_kpoints = (0..<total_keypoint_number).map { _ -> PredictedPoint? in
             return nil
         }
         
@@ -28,6 +36,8 @@ class HeatmapPostProcessor {
                 for j in 0..<heatmap_h {
                     let index = k*(heatmap_w*heatmap_h) + i*(heatmap_h) + j
                     let confidence = heatmaps[index].doubleValue
+                    //if maxvalue < confidence { maxvalue = confidence }
+                    //if minvalue > confidence { minvalue = confidence }
                     guard confidence > 0 else { continue }
                     if n_kpoints[k] == nil ||
                         (n_kpoints[k] != nil && n_kpoints[k]!.maxConfidence < confidence) {
@@ -37,11 +47,15 @@ class HeatmapPostProcessor {
             }
         }
         
+        //print(maxvalue, minvalue)
+        
         // transpose to (1.0, 1.0)
         n_kpoints = n_kpoints.map { kpoint -> PredictedPoint? in
             if let kp = kpoint {
-                return PredictedPoint(maxPoint: CGPoint(x: (kp.maxPoint.x+0.5)/CGFloat(heatmap_w),
-                                                        y: (kp.maxPoint.y+0.5)/CGFloat(heatmap_h)),
+                var x: CGFloat = (kp.maxPoint.x+0.5)/CGFloat(heatmap_w)
+                let y: CGFloat = (kp.maxPoint.y+0.5)/CGFloat(heatmap_h)
+                if isFlipped { x = 1 - x }
+                return PredictedPoint(maxPoint: CGPoint(x: x, y: y),
                                       maxConfidence: kp.maxConfidence)
             } else {
                 return nil
--- PoseEstimation-CoreML/Pose Matching View Controller/PoseMatchingViewController.swift
+++ PoseEstimation-CoreML/Pose Matching View Controller/PoseMatchingViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 import CoreMedia
+import Vision
 
 class PoseMatchingViewController: UIViewController {
 
     // MARK: - UI Property
     @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var jointView: DrawingJointView!
+    @IBOutlet var capturedJointViews: [DrawingJointView]!
+    
+    var capturedIndex = 0
     
     // MARK: - AV Property
     var videoCapture: VideoCapture!
     
+    // MARK: - ML Properties
+    // Core ML model
+    typealias EstimationModel = model_cpm
+    
+    // Preprocess and Inference
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    
+    // Postprocess
+    var postProcessor: HeatmapPostProcessor = HeatmapPostProcessor()
+    var mvfilters: [MovingAverageFilter] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // setup the drawing views
+        setUpCapturedJointView()
 
+        // setup the model
+        setUpModel()
+        
         // setup camera
         setUpCamera()
     }
@@ -36,6 +59,27 @@ class PoseMatchingViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup Captured Joint View
+    func setUpCapturedJointView() {
+        postProcessor.onlyBust = true
+        
+        for capturedJointView in capturedJointViews {
+            capturedJointView.layer.borderWidth = 2
+            capturedJointView.layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        if let visionModel = try? VNCoreMLModel(for: EstimationModel().model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .centerCrop
+        } else {
+            fatalError("cannot load the ml model")
+        }
     }
     
     // MARK: - SetUp Video
@@ -66,6 +110,15 @@ class PoseMatchingViewController: UIViewController {
     func resizePreviewLayer() {
         videoCapture.previewLayer?.frame = videoPreview.bounds
     }
+    
+    @IBAction func tapCapture(_ sender: Any) {
+        let currentIndex = capturedIndex % capturedJointViews.count
+        let capturedJointView = capturedJointViews[currentIndex]
+        
+        capturedJointView.bodyPoints = jointView.bodyPoints
+        
+        capturedIndex += 1
+    }
 }
 
 // MARK: - VideoCaptureDelegate
@@ -73,7 +126,48 @@ extension PoseMatchingViewController: VideoCaptureDelegate {
     func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?, timestamp: CMTime) {
         // the captured image from camera is contained on pixelBuffer
         if let pixelBuffer = pixelBuffer {
-            // <#TODO#>
+            predictUsingVision(pixelBuffer: pixelBuffer)
         }
+    }
+}
+
+extension PoseMatchingViewController {
+    // MARK: - Inferencing
+    func predictUsingVision(pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        // vision framework configures the input size of image following our model's input configuration automatically
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        try? handler.perform([request])
+    }
+    
+    // MARK: - Postprocessing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNCoreMLFeatureValueObservation],
+            let heatmaps = observations.first?.featureValue.multiArrayValue else { return }
+        
+        /* =================================================================== */
+        /* ========================= post-processing ========================= */
+        
+        /* ------------------ convert heatmap to point array ----------------- */
+        var predictedPoints = postProcessor.convertToPredictedPoints(from: heatmaps, isFlipped: true)
+        
+        /* --------------------- moving average filter ----------------------- */
+        if predictedPoints.count != mvfilters.count {
+            mvfilters = predictedPoints.map { _ in MovingAverageFilter(limit: 3) }
+        }
+        for (predictedPoint, filter) in zip(predictedPoints, mvfilters) {
+            filter.add(element: predictedPoint)
+        }
+        predictedPoints = mvfilters.map { $0.averagedValue() }
+        /* =================================================================== */
+        
+        /* =================================================================== */
+        /* ======================= display the results ======================= */
+        DispatchQueue.main.sync { [weak self] in
+            guard let self = self else { return }
+            // draw line
+            self.jointView.bodyPoints = predictedPoints
+        }
+        /* =================================================================== */
     }
 }
